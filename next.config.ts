2ff62085227efafefb498cd0b651e3f31e6a8d32
lantemir <temiros@mail.ru>
2025-07-19 10:02:13 +0500

    NextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,25 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+let imageHost = 'localhost';
+try {
+  imageHost = new URL(process.env.NEXT_PUBLIC_API_URL || '').hostname;
+} catch (e) {
+  console.warn('⚠️ Invalid NEXT_PUBLIC_API_URL, fallback to "localhost"');
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false, // ❗️Отключает двойной рендер
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
-        pathname: '/media/**',
-      },
-    ],
+    remotePatterns: 
+       [
+        {
+          protocol: isProd ? 'https': 'http',
+          hostname: imageHost,       
+          pathname: '/media/**',
+        },
+      ]    
   },
   eslint: {
     ignoreDuringBuilds: true,
